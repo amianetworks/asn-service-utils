3297faedd54456b75c6912a8e07aa92ce0354316
Terry Du <terrydurunkai@gmail.com>
2025-09-29 10:12:13 -0700

    Update dependencies and Docker images to version 25.9.4

--- docker/cluster.go
+++ docker/cluster.go
@@ -402,7 +402,7 @@ func main() {
 	}
 	asncD.Services["sapphire-iam"] = DockerService{
 		ContainerName: "sapphire-iam",
-		Image:         "registry.amiasys.com/sapphire.iam:25.9.0",
+		Image:         "registry.amiasys.com/sapphire.iam:25.9.1",
 		Restart:       "always",
 		Privileged:    true,
 		DependsOn:     []string{"asn-mdb"},
@@ -414,7 +414,7 @@ func main() {
 		},
 	}
 	asncD.Services["asnc"] = DockerService{
-		Image:       "registry.amiasys.com/asnc:25.9.3",
+		Image:       "registry.amiasys.com/asnc:25.9.4",
 		Restart:     "always",
 		DependsOn:   []string{"asn-mdb", "asn-idb", "sapphire-iam"},
 		NetworkMode: "host",
@@ -528,7 +528,7 @@ func main() {
 		asnD := asncDocker{
 			Services: map[string]DockerService{
 				"asnsn": {
-					Image:         "registry.amiasys.com/asnsn:25.9.3",
+					Image:         "registry.amiasys.com/asnsn:25.9.4",
 					ContainerName: fmt.Sprintf("network-node%d-switch%d", i, i),
 					Restart:       "always",
 					Volumes: []string{
--- go.mod
+++ go.mod
@@ -2,11 +2,11 @@ module asn.amiasys.com
 
 go 1.24.0
 
-replace asn.amiasys.com/asn-service-api/v25 => github.com/amianetworks/asn-service-api/v25 v25.9.3
+replace asn.amiasys.com/asn-service-api/v25 => github.com/amianetworks/asn-service-api/v25 v25.9.4
 
 require (
-	asn.amiasys.com/asn-service-api/v25 v25.9.3
-	github.com/amianetworks/am.modules/v25 v25.9.2
+	asn.amiasys.com/asn-service-api/v25 v25.9.4
+	github.com/amianetworks/am.modules/v25 v25.9.3
 	github.com/gin-gonic/gin v1.10.1
 	github.com/goccy/go-json v0.10.5
 	github.com/golang-jwt/jwt/v5 v5.3.0
