96f41180d1f34ea5177d1657335e528b508dd558
Terry Du <terrydurunkai@gmail.com>
2025-10-29 10:34:24 -0700

    Update dependencies and Docker images to version 25.11.4

--- docker/cluster.go
+++ docker/cluster.go
@@ -422,7 +422,7 @@ func main() {
 		},
 	}
 	asncD.Services["asnc"] = DockerService{
-		Image:       "registry.amiasys.com/asnc:25.11.3",
+		Image:       "registry.amiasys.com/asnc:25.11.4",
 		Restart:     "always",
 		DependsOn:   []string{"asn-mdb", "asn-idb", "asn-rdb", "sapphire-iam"},
 		NetworkMode: "host",
@@ -536,7 +536,7 @@ func main() {
 		asnD := asncDocker{
 			Services: map[string]DockerService{
 				"asnsn": {
-					Image:         "registry.amiasys.com/asnsn:25.11.3",
+					Image:         "registry.amiasys.com/asnsn:25.11.4",
 					ContainerName: fmt.Sprintf("network-node%d-switch%d", i, i),
 					Restart:       "always",
 					Volumes: []string{
