e06e2863c67c1d297be22cc69ff94dbcc07e6856
Terry Du <terrydurunkai@gmail.com>
2026-02-05 13:04:03 -0800

    Bump ASNC, ASNSN, and Sapphire-IAM Docker images to version 26.3.x and update ASN version accordingly

--- docker/cluster.go
+++ docker/cluster.go
@@ -196,7 +196,7 @@ func main() {
 	}
 	asncD.Services["sapphire-iam"] = DockerService{
 		ContainerName: "sapphire-iam",
-		Image:         "registry.amiasys.com/sapphire.iam:26.2.2",
+		Image:         "registry.amiasys.com/sapphire.iam:26.3.1",
 		NetworkMode:   "host",
 		Restart:       "unless-stopped",
 		Privileged:    true,
@@ -212,7 +212,7 @@ func main() {
 		},
 	}
 	asncD.Services["asnc"] = DockerService{
-		Image:       "registry.amiasys.com/asnc:26.2.1",
+		Image:       "registry.amiasys.com/asnc:26.3.0",
 		Restart:     "unless-stopped",
 		DependsOn:   []string{"asn-mdb", "asn-idb", "asn-rdb", "sapphire-iam"},
 		NetworkMode: "host",
@@ -288,7 +288,7 @@ func main() {
 		}
 
 		asnD.Services[fmt.Sprintf("asnsn-%d", i)] = DockerService{
-			Image:         "registry.amiasys.com/asnsn:26.2.1",
+			Image:         "registry.amiasys.com/asnsn:26.3.0",
 			ContainerName: fmt.Sprintf("asnsn-%d", i),
 			NetworkMode:   "host",
 			Environment: map[string]string{
