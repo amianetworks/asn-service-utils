2d2f39d54fc43bf12e54f3f8045fd7a8a9dbbb18
Terry Du <terrydurunkai@gmail.com>
2026-06-10 11:41:23 -0700

    Bump ASN service API to v26.7.4 and Sapphire IAM to v26.7.3 across configuration files, scripts, and Docker images.

--- docker/cluster.go
+++ docker/cluster.go
@@ -196,7 +196,7 @@ func main() {
 	}
 	asncD.Services["sapphire-iam"] = DockerService{
 		ContainerName: "sapphire-iam",
-		Image:         "registry.amiasys.com/sapphire.iam:26.7.1",
+		Image:         "registry.amiasys.com/sapphire.iam:26.7.3",
 		NetworkMode:   "host",
 		Restart:       "unless-stopped",
 		Privileged:    true,
