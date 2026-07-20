c0b684ce2f5a54ef41e600dc19cb7915dfa94d18
Terry Du <terrydurunkai@gmail.com>
2026-07-20 14:10:51 -0700

    Bump ASN version to 26.8.3

--- docker/cluster.go
+++ docker/cluster.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"os"
 	"strconv"
-
-	"gopkg.in/yaml.v3"
 )
 
 type ASNC struct {
@@ -212,7 +210,7 @@ func main() {
 		},
 	}
 	asncD.Services["asnc"] = DockerService{
-		Image:       "registry.amiasys.com/asnc:26.8.0",
+		Image:       "registry.amiasys.com/asnc:26.8.3",
 		Restart:     "unless-stopped",
 		DependsOn:   []string{"asn-mdb", "asn-idb", "asn-rdb", "sapphire-iam"},
 		NetworkMode: "host",
@@ -288,7 +286,7 @@ func main() {
 		}
 
 		asnD.Services[fmt.Sprintf("asnsn-%d", i)] = DockerService{
-			Image:         "registry.amiasys.com/asnsn:26.8.0",
+			Image:         "registry.amiasys.com/asnsn:26.8.3",
 			ContainerName: fmt.Sprintf("asnsn-%d", i),
 			NetworkMode:   "host",
 			Environment: map[string]string{
--- workflow/CheckPlanTargetMigration.md
+++ workflow/CheckPlanTargetMigration.md
@@ -135,7 +135,7 @@ Expected framework output:
 
 ```text
 >> Version Identity
-  ASN Framework : 26.8.0
+  ASN Framework : 26.8.3
    Go Toolchain : 1.26.3
 ```
 
